f8f21bd5a15a94d46f2f475a5c9e47347018fc51
G Sriram <sgcuber24@gmail.com>
2020-09-29 15:55:32 +0530

    Completed

--- lib/screens/substitute_faculty_screen.dart
+++ lib/screens/substitute_faculty_screen.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:substitute_teacher_management/widgets/faculty_management/facultyTile.dart';
 
 class SubstituteFacultyScreen extends StatefulWidget {
   static const routeName = '/substitute-faculty-screen';
@@ -10,10 +12,11 @@ class SubstituteFacultyScreen extends StatefulWidget {
 }
 
 class _SubstituteFacultyScreenState extends State<SubstituteFacultyScreen> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
   var currentFaculty;
   var _isInit = true;
   var _isLoading = false;
-
+  var day = DateFormat.EEEE().format(DateTime.now());
   List allFaculties;
   Future<List> getFaculties() async {
     QuerySnapshot faculties =
@@ -29,22 +32,214 @@ class _SubstituteFacultyScreenState extends State<SubstituteFacultyScreen> {
         .toList();
   }
 
+  int getFreeHours(faculty, day) {
+    return faculty['timeTable'][day].where((c) => c == 'Free').toList().length;
+  }
+
+  List getClasses(faculty) {
+    List classes = [];
+    faculty['classes'].forEach((cls) {
+      classes.add(cls['semester'] + cls['branch'] + cls['section']);
+    });
+    return classes;
+  }
+
+  Map getClass(String clss, faculty) {
+    var foundClass = {};
+    faculty['classes'].forEach((cls) {
+      if (cls['id'] == clss) {
+        foundClass = cls;
+      }
+    });
+    return foundClass;
+  }
+
+  List findSubstituteFaculties(day, hour) {
+    final dailyTimeTable = currentFaculty['timeTable'][day];
+    final hourClass = dailyTimeTable[hour];
+    final freeFacultyList = [];
+    allFaculties.forEach((faculty) {
+      var classObj = getClass(hourClass, currentFaculty);
+      var cls = classObj['semester'] + classObj['branch'] + classObj['section'];
+      if (getClasses(faculty).contains(cls) &&
+          faculty['empId'] != currentFaculty['empId']) {
+        if (faculty['timeTable'][day][hour] == 'Free') {
+          freeFacultyList.add(faculty);
+        }
+      }
+    });
+    return freeFacultyList;
+  }
+
   @override
   void didChangeDependencies() async {
     if (_isInit) {
       currentFaculty = ModalRoute.of(context).settings.arguments as Map;
       allFaculties = await getFaculties();
     }
-    _isInit = false;
+    setState(() {
+      _isInit = false;
+    });
+
     super.didChangeDependencies();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text(currentFaculty['fullName']),
       ),
+      body: _isInit
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : Padding(
+              padding: EdgeInsets.all(16.0),
+              child: ListView(
+                children: [
+                  Text(
+                    'Day',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(16),
+                      child: DropdownButtonFormField(
+                        icon: Icon(Icons.expand_more),
+                        items: [
+                          'Monday',
+                          'Tuesday',
+                          'Wednesday',
+                          'Thursday',
+                          'Friday',
+                          'Saturday'
+                        ]
+                            .map<DropdownMenuItem<String>>(
+                              (d) => DropdownMenuItem<String>(
+                                value: d,
+                                child: Text(d),
+                              ),
+                            )
+                            .toList(),
+                        onChanged: (value) {
+                          setState(() {
+                            day = value;
+                          });
+                        },
+                        value: day,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  currentFaculty['timeTable'][day]
+                              .where((i) => i != 'Free')
+                              .toList()
+                              .length ==
+                          0
+                      ? Container(
+                          height: 400,
+                          child: Center(
+                            child: Text(
+                              'Faculty is Free!',
+                              style: Theme.of(context).textTheme.title,
+                            ),
+                          ),
+                        )
+                      : Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Substitute Faculties',
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 16,
+                            ),
+                            Card(
+                              child: Padding(
+                                padding: EdgeInsets.all(16),
+                                child: Column(
+                                  children: currentFaculty['timeTable'][day]
+                                      .asMap()
+                                      .entries
+                                      .map<Widget>(
+                                        (hour) {
+                                          if (hour.value != 'Free') {
+                                            var cls;
+                                            var classObj = getClass(
+                                                hour.value, currentFaculty);
+                                            cls = classObj['semester'] +
+                                                ' ' +
+                                                classObj['section'] +
+                                                ' ' +
+                                                classObj['branch'];
+                                            var subFaculties =
+                                                findSubstituteFaculties(
+                                                    day, hour.key);
+                                            return ExpansionTile(
+                                              childrenPadding:
+                                                  EdgeInsets.symmetric(
+                                                vertical: 16,
+                                              ),
+                                              title: Text(
+                                                  'Hour ${(hour.key + 1)}: $cls'),
+                                              children: subFaculties.length == 0
+                                                  ? [
+                                                      Text(
+                                                        'No Free Faculties',
+                                                        style: TextStyle(
+                                                          fontSize: 15,
+                                                          fontWeight:
+                                                              FontWeight.bold,
+                                                        ),
+                                                      )
+                                                    ]
+                                                  : subFaculties
+                                                      .map(
+                                                        (faculty) =>
+                                                            FacultyTile(
+                                                          faculty,
+                                                          scaffoldKey:
+                                                              _scaffoldKey,
+                                                          isSubstituteScreen:
+                                                              true,
+                                                          cls: cls,
+                                                          hour: hour.key,
+                                                          numFreeHrs:
+                                                              getFreeHours(
+                                                            faculty,
+                                                            day,
+                                                          ),
+                                                        ),
+                                                      )
+                                                      .toList(),
+                                            );
+                                          }
+                                        },
+                                      )
+                                      .where((o) => o != null)
+                                      .toList(),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                ],
+              ),
+            ),
     );
   }
 }
--- lib/widgets/faculty_management/facultyTile.dart
+++ lib/widgets/faculty_management/facultyTile.dart
@@ -1,77 +1,152 @@
 import 'package:flutter/material.dart';
+import 'package:mailer/smtp_server.dart';
+import 'package:mailer/mailer.dart';
 import 'package:substitute_teacher_management/screens/edit_faculty_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:substitute_teacher_management/screens/substitute_faculty_screen.dart';
+import '../../email_credentials.dart';
 
-class FacultyTile extends StatelessWidget {
+class FacultyTile extends StatefulWidget {
   final dynamic faculty;
   final bool isHomeScreen;
-  FacultyTile(this.faculty, {this.isHomeScreen = false});
+  final bool isSubstituteScreen;
+  final int numFreeHrs;
+  final scaffoldKey;
+  final int hour;
+  final String cls;
+  FacultyTile(this.faculty,
+      {this.scaffoldKey,
+      this.isHomeScreen = false,
+      this.isSubstituteScreen = false,
+      this.numFreeHrs = 0,
+      this.hour,
+      this.cls});
+
+  @override
+  _FacultyTileState createState() => _FacultyTileState();
+}
+
+class _FacultyTileState extends State<FacultyTile> {
+  var _isLoading = false;
+
+  final successSnackBar =
+      SnackBar(content: Text('Message sent!', textAlign: TextAlign.center));
+
+  final failSnackBar = SnackBar(
+      content: Text('Error sending message!', textAlign: TextAlign.center));
+
+  Future<void> _sendEmail() async {
+    setState(() {
+      _isLoading = true;
+    });
+    final smtpServer = gmail(username, password);
+
+    // Create our message.
+    final message = Message()
+      ..from = Address(username, 'Admin')
+      ..recipients.add(widget.faculty['email'])
+      ..subject = 'Substitution class for ${widget.cls}'
+      ..html =
+          "<h1>New Substitution Class Allotted</h1>\n<p>Class: ${widget.cls}</p>\n<p>Hour: ${widget.hour + 1}</p>";
+
+    try {
+      final sendReport = await send(message, smtpServer);
+      setState(() {
+        _isLoading = false;
+      });
+      widget.scaffoldKey.currentState.showSnackBar(successSnackBar);
+      print('Message sent: ' + sendReport.toString());
+    } on MailerException catch (e) {
+      setState(() {
+        _isLoading = false;
+      });
+      print(e);
+      widget.scaffoldKey.currentState.showSnackBar(failSnackBar);
+      print('Message not sent.');
+      for (var p in e.problems) {
+        print('Problem: ${p.code}: ${p.msg}');
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final scaffold = Scaffold.of(context);
     return Container(
-      child: ListTile(
-        onTap: isHomeScreen
-            ? () {
-                Navigator.of(context).pushNamed(
-                  SubstituteFacultyScreen.routeName,
-                  arguments: faculty,
-                );
-              }
-            : null,
-        title: Text(faculty['fullName']),
-        leading: CircleAvatar(
-          backgroundColor: Theme.of(context).primaryColor,
-          child: Text(faculty['empId']),
-        ),
-        // subtitle: Row(
-        //   children: faculty['classes'].map<Widget>(
-        //     (sub) {
-        //       return Text(sub['subjectName'] + ', ');
-        //     },
-        //   ).toList(),
-        // ),
-        trailing: isHomeScreen
-            ? null
-            : Container(
-                width: 100,
-                child: Row(
-                  children: <Widget>[
-                    IconButton(
-                      icon: Icon(Icons.edit),
-                      onPressed: () {
-                        Navigator.of(context).pushNamed(
-                            EditFacultyScreen.routeName,
-                            arguments: faculty);
-                      },
-                      color: Theme.of(context).primaryColor,
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.delete),
-                      onPressed: () async {
-                        try {
-                          FirebaseFirestore.instance
-                              .collection('faculties')
-                              .doc(faculty['empId'])
-                              .delete();
-                        } catch (error) {
-                          scaffold.showSnackBar(
-                            SnackBar(
-                              content: Text(
-                                'Deleting failed!',
-                                textAlign: TextAlign.center,
-                              ),
-                            ),
-                          );
-                        }
-                      },
-                      color: Theme.of(context).errorColor,
-                    ),
-                  ],
-                ),
+      child: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListTile(
+              onTap: widget.isHomeScreen
+                  ? () {
+                      Navigator.of(context).pushNamed(
+                        SubstituteFacultyScreen.routeName,
+                        arguments: widget.faculty,
+                      );
+                    }
+                  : null,
+              title: Text(widget.faculty['fullName']),
+              leading: CircleAvatar(
+                backgroundColor: Theme.of(context).primaryColor,
+                child: Text(widget.faculty['empId']),
               ),
-      ),
+              subtitle: widget.isSubstituteScreen
+                  ? Text(
+                      'Free hours today: ${widget.numFreeHrs}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                      ),
+                    )
+                  : null,
+              trailing: widget.isHomeScreen
+                  ? null
+                  : widget.isSubstituteScreen
+                      ? IconButton(
+                          icon: Icon(Icons.email),
+                          onPressed: () async {
+                            await _sendEmail();
+                          },
+                          color: Theme.of(context).errorColor,
+                        )
+                      : Container(
+                          width: 100,
+                          child: Row(
+                            children: <Widget>[
+                              IconButton(
+                                icon: Icon(Icons.edit),
+                                onPressed: () {
+                                  Navigator.of(context).pushNamed(
+                                      EditFacultyScreen.routeName,
+                                      arguments: widget.faculty);
+                                },
+                                color: Theme.of(context).primaryColor,
+                              ),
+                              IconButton(
+                                icon: Icon(Icons.delete),
+                                onPressed: () async {
+                                  try {
+                                    FirebaseFirestore.instance
+                                        .collection('faculties')
+                                        .doc(widget.faculty['empId'])
+                                        .delete();
+                                  } catch (error) {
+                                    scaffold.showSnackBar(
+                                      SnackBar(
+                                        content: Text(
+                                          'Deleting failed!',
+                                          textAlign: TextAlign.center,
+                                        ),
+                                      ),
+                                    );
+                                  }
+                                },
+                                color: Theme.of(context).errorColor,
+                              ),
+                            ],
+                          ),
+                        ),
+            ),
     );
   }
 }
